cc47dbb8778dec94ed71313424cb1906e96a4724
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-28 11:25:08 +0000

    wrote third failing test

--- spec/school_report_spec.rb
+++ spec/school_report_spec.rb
@@ -2,12 +2,18 @@ require 'school_report'
 
 describe SchoolReport do
   describe '#format' do
-    it 'should return the correcct score for green when given green' do
+    it 'should return the correct score for green when given green' do
       expect(subject.format("Green")).to eq "Green: 1"
     end
 
     it 'should return the correct score for green when given green twice' do
       expect(subject.format("Green, Green")).to eq "Green: 2"
     end
+
+    it 'should return the correct score for green and amber when given green once and amber once' do
+      expect(subject.format("Green, Amber")).to eq "Green: 1\nAmber: 1"
+    end
+
+
   end
 end
